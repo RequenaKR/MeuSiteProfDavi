3de6b5ecead84608fb3cc7563cbce7c54404cbbe
RequenaKR <kaiorequena2009@gmail.com>
2026-09-08 10:17:29 -0300

    fim
    coloquei todos os 22 filmes e 22 series, site pronto

--- filmes.js
+++ filmes.js
@@ -16,6 +16,95 @@ export const filmes = [
     },
 
     {
+        id: "Michael",
+        titulo: "Michael",
+        imagem: "Imagens/michael.jpg",
+        descricao: "Michael é uma cinebiografia musical de 2026 dirigida por Antoine Fuqua que retrata a vida e a carreira do cantor Michael Jackson.",
+        trailer: "u1HARE7V4UU?si=uGoiikvCDmmex-Zm",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h 7m",
+        genero: "Musical/Drama",
+        classificacao: "12",
+    },
+
+    
+    {
+        id: "Como Eu Era Antes de Você",
+        titulo: "Como Eu Era Antes de Você",
+        imagem: "Imagens/como.jpg",
+        descricao: "Como Eu Era Antes de Você é um drama romântico de 2016 que mistura momentos de humor leve com uma carga dramática intensa, baseado no livro homônimo de Jojo Moyes.",
+        trailer: "PnqUs3xiAVI?si=pMrS2f-U_cyTtKDK",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "1h 50m",
+        genero: "Romance/Drama",
+        classificacao: "12",
+    },
+
+    
+    {
+        id: "Sonic",
+        titulo: "Sonic",
+        imagem: "Imagens/sonic.png",
+        descricao: "Em Sonic 3 - O Filme, Sonic, Tails e Knuckles se unem para enfrentar um novo e poderoso rival chamado Shadow.",
+        trailer: "-bcjtApoUyU?si=S33Y5kbpDKAkf4d3",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "1h 50m",
+        genero: "Comédia/Aventura",
+        classificacao: "10",
+    },
+
+    
+    {
+        id: "Jumanji",
+        titulo: "Jumanji",
+        imagem: "Imagens/juman.jpg",
+        descricao: "Jumanji é uma famosa franquia de aventura que começou com um livro infantil de 1982 e virou um sucesso no cinema com Jumanji (1995), estrelado por Robin Williams.",
+        trailer: "CYc9CXGu9BI?si=NggBen0r4rx7sn_Z",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h 3m",
+        genero: " Comédia/Ação",
+        classificacao: "12",
+    },
+
+    
+    {
+        id: "A Empregada",
+        titulo: "A Empregada",
+        imagem: "Imagens/empregada.jpg",
+        descricao: "A Empregada é um thriller psicológico de sucesso escrito por Freida McFadden e adaptado para o IMDb.",
+        trailer: "Nsm0qczuTZk?si=ZOn1dVJk9i59FJls",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h 13m",
+        genero: "Thriller/Mistério",
+        classificacao: "16",
+    },
+
+    
+    {
+        id: "Coringa",
+        titulo: "Coringa",
+        imagem: "Imagens/coringa.jpg",
+        descricao: "O filme Coringa (2019), dirigido por Todd Phillips, é um drama psicológico sombrio que explora as origens do icônico vilão da DC através da história de Arthur Fleck (Joaquin Phoenix).",
+        trailer: "jfVTJm9NilA?si=jnAk6s5m5VS6sUt5",
+
+        // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
+
+        duracao: "2h 2m",
+        genero: "Crime/Thriller",
+        classificacao: "16",
+    },
+
+    {
         id: "Batman",
         titulo: "Batman",
         imagem: "Imagens/batman.png",
@@ -83,6 +172,128 @@ export const filmes = [
         duracao: "2h 28m",
         genero: "Ação/Aventura",
         classificacao: "12",
+    },
+
+    // NOVOS FILMES
+
+    {
+        id: "Ilha do Medo",
+        titulo: "Ilha do Medo",
+        imagem: "Imagens/im.jpg",
+        descricao: "Dois agentes investigam o desaparecimento de uma paciente em um hospital psiquiátrico localizado em uma ilha isolada.",
+        trailer: "gL4rP2prdfg?si=Hb56lDATLJV8nS51",
+
+        duracao: "2h 18m",
+        genero: "Suspense/Mistério",
+        classificacao: "16",
+    },
+
+    {
+        id: "O Poderoso Chefão",
+        titulo: "O Poderoso Chefão",
+        imagem: "Imagens/poderoso.png",
+        descricao: "A história de uma poderosa família envolvida no mundo do crime organizado e das disputas pelo controle de seus negócios.",
+        trailer: "SaHZHU-44XA?si=JXFZvJoy9xwOgXKo",
+
+        duracao: "2h 55m",
+        genero: "Crime/Drama",
+        classificacao: "16",
+    },
+
+    {
+        id: "Matrix",
+        titulo: "Matrix",
+        imagem: "Imagens/matrix.jpg",
+        descricao: "Um programador descobre que a realidade que conhece é uma grande simulação e começa uma jornada para descobrir a verdade.",
+        trailer: "mPYfd6PCmYY?si=sQk11C_OgMasHW_Q",
+
+        duracao: "2h 16m",
+        genero: "Ficção científica/Ação",
+        classificacao: "14",
+    },
+
+    {
+        id: "Vingadores Ultimato",
+        titulo: "Vingadores: Ultimato",
+        imagem: "Imagens/avengers.jpg",
+        descricao: "Os heróis restantes tentam encontrar uma maneira de reverter os acontecimentos que mudaram completamente o universo.",
+        trailer: "PgrmbRID0eY?si=MIF3qmtB_EXw5QJq",
+
+        duracao: "3h 1m",
+        genero: "Ação/Aventura",
+        classificacao: "12",
+    },
+
+    {
+        id: "Harry Potter e a Pedra Filosofal",
+        titulo: "Harry Potter e a Pedra Filosofal",
+        imagem: "Imagens/hp.jpg",
+        descricao: "Um jovem descobre que é um bruxo e começa seus estudos em uma escola de magia, onde vive grandes aventuras.",
+        trailer: "SFzft_2dcV0?si=YSzAXq_n7kwOyXhS",
+
+        duracao: "2h 32m",
+        genero: "Fantasia/Aventura",
+        classificacao: "Livre",
+    },
+
+    {
+        id: "O Senhor dos Anéis",
+        titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+        imagem: "Imagens/sa.jpg",
+        descricao: "Um jovem hobbit recebe a missão de destruir um poderoso anel antes que ele caia nas mãos de seu criador.",
+        trailer: "0i86oM1nHjM?si=HCBozp4CAHjWRbBv",
+
+        duracao: "2h 58m",
+        genero: "Fantasia/Aventura",
+        classificacao: "12",
+    },
+
+    {
+        id: "Jurassic Park",
+        titulo: "Jurassic Park",
+        imagem: "Imagens/jp.jpg",
+        descricao: "Um parque temático com dinossauros recriados geneticamente sai do controle e coloca seus visitantes em perigo.",
+        trailer: "bx46tthKXmc?si=MCkKMpvxbUj0QR9N",
+
+        duracao: "2h 7m",
+        genero: "Aventura/Ficção científica",
+        classificacao: "10",
+    },
+
+    {
+        id: "Titanic",
+        titulo: "Titanic",
+        imagem: "Imagens/titanic.jpg",
+        descricao: "Durante a viagem inaugural do famoso navio, dois jovens de classes sociais diferentes se conhecem e vivem uma história marcante.",
+        trailer: "IH6_CA_ocqY?si=kIorkOzMe_rts6Th",
+
+        duracao: "3h 14m",
+        genero: "Romance/Drama",
+        classificacao: "12",
+    },
+
+    {
+        id: "Forrest Gump",
+        titulo: "Forrest Gump",
+        imagem: "Imagens/fg.jpg",
+        descricao: "A vida de um homem simples que acaba participando de diversos acontecimentos importantes da história dos Estados Unidos.",
+        trailer: "bLvqoHBptjg?si=zPHRWPEPyJ4hBs2X",
+
+        duracao: "2h 22m",
+        genero: "Drama/Romance",
+        classificacao: "14",
+    },
+
+    {
+        id: "Oppenheimer",
+        titulo: "Oppenheimer",
+        imagem: "Imagens/opp.jpg",
+        descricao: "A trajetória do cientista J. Robert Oppenheimer e seu papel no desenvolvimento de uma das armas mais importantes da história.",
+        trailer: "F3OxA9Cz17A?si=EnGMQ0FNpYHFMvcH",
+
+        duracao: "3h",
+        genero: "Biografia/Drama",
+        classificacao: "16",
     }
 ];
 
@@ -164,5 +375,199 @@ export const series = [
         duracao: "3 temporadas",
         genero: "Aventura",
         classificacao: "10",
+    },
+
+    // NOVAS SÉRIES
+
+    {
+        id: "The Walking Dead",
+        titulo: "The Walking Dead",
+        imagem: "Imagens/twd.jpg",
+        descricao: "Um grupo de sobreviventes tenta encontrar segurança e reconstruir suas vidas em um mundo transformado por uma epidemia.",
+        trailer: "cctvSNDaaYs?si=ZFJ67p-wrSPqqngn",
+
+        duracao: "11 temporadas",
+        genero: "Drama/Terror",
+        classificacao: "16",
+    },
+
+    {
+        id: "The Last of Us",
+        titulo: "The Last of Us",
+        imagem: "Imagens/the.jpg",
+        descricao: "Um homem e uma jovem atravessam os Estados Unidos em busca de segurança após uma grande pandemia.",
+        trailer: "IcR-6y3Q9XI?si=JmlInjtvGrTVupqg",
+
+        duracao: "2 temporadas",
+        genero: "Drama/Aventura",
+        classificacao: "16",
+    },
+
+    {
+        id: "La Casa de Papel",
+        titulo: "La Casa de Papel",
+        imagem: "Imagens/lcp.webp",
+        descricao: "Um grupo de criminosos participa de um grande plano organizado por um homem conhecido como Professor.",
+        trailer: "jkzl8ZAppqE?si=t3xQHXl6dsIaDOoi",
+
+        duracao: "5 partes",
+        genero: "Crime/Drama",
+        classificacao: "16",
+    },
+
+    {
+        id: "The Boys",
+        titulo: "The Boys",
+        imagem: "Imagens/tboys.jpg",
+        descricao: "Um grupo de pessoas decide enfrentar super-heróis que usam seus poderes de maneira irresponsável.",
+        trailer: "I0RC15nmjpU?si=bxAOIan2Fi0S5AP0",
+
+        duracao: "5 temporadas",
+        genero: "Ação/Drama",
+        classificacao: "18",
+    },
+
+    {
+        id: "The Witcher",
+        titulo: "The Witcher",
+        imagem: "Imagens/tw.jpg",
+        descricao: "Um caçador de monstros viaja por um mundo de fantasia enfrentando criaturas e conflitos políticos.",
+        trailer: "SB3siHVCHpY?si=3r8Hhtr6CYPSAfZ2",
+
+        duracao: "4 temporadas",
+        genero: "Fantasia/Ação",
+        classificacao: "16",
+    },
+
+    {
+        id: "The Office",
+        titulo: "The Office",
+        imagem: "Imagens/office.jpg",
+        descricao: "Uma equipe de funcionários trabalha em um escritório enquanto enfrenta situações engraçadas e inesperadas no dia a dia.",
+        trailer: "tNcDHWpselE?si=jpvY4ggyVNRR6ZXS",
+
+        duracao: "9 temporadas",
+        genero: "Comédia",
+        classificacao: "12",
+    },
+
+    {
+        id: "Friends",
+        titulo: "Friends",
+        imagem: "Imagens/amigos.jpg",
+        descricao: "Seis amigos vivem juntos diversas situações envolvendo amizade, trabalho, relacionamentos e vida adulta.",
+        trailer: "OhfKo8mDCa8?si=VN9osL7_gZ3nUwKh",
+
+        duracao: "10 temporadas",
+        genero: "Comédia/Romance",
+        classificacao: "12",
+    },
+
+    {
+        id: "The Mandalorian",
+        titulo: "The Mandalorian",
+        imagem: "Imagens/manda.jpg",
+        descricao: "Um caçador de recompensas viaja pela galáxia enquanto protege uma criança muito especial.",
+        trailer: "IHWlvwu8t1w?si=JEPJYTr_5m-7rTEr",
+
+        duracao: "3 temporadas",
+        genero: "Ficção científica/Ação",
+        classificacao: "10",
+    },
+
+    {
+        id: "Wandinha",
+        titulo: "Wandinha",
+        imagem: "Imagens/wandinha.jpg",
+        descricao: "Wandinha Addams começa a estudar em uma escola especial e se envolve em acontecimentos misteriosos.",
+        trailer: "4vi6ZWe_3Yc?si=_8DpVv2FkDAdgEXk",
+
+        duracao: "2 temporadas",
+        genero: "Comédia/Mistério",
+        classificacao: "12",
+    },
+
+    {
+        id: "The Umbrella Academy",
+        titulo: "The Umbrella Academy",
+        imagem: "Imagens/tua.jpg",
+        descricao: "Irmãos com poderes especiais se reencontram para tentar impedir uma ameaça que pode destruir o mundo.",
+        trailer: "5_4SW8HHfUs?si=vPJmcqGbi2vi9f8w",
+
+        duracao: "4 temporadas",
+        genero: "Ação/Fantasia",
+        classificacao: "16",
+    },
+
+    {
+        id: "Cobra Kai",
+        titulo: "Cobra Kai",
+        imagem: "Imagens/kai.webp",
+        descricao: "Antigos rivais retomam a rivalidade através de seus dojos de karatê, envolvendo também uma nova geração de alunos.",
+        trailer: "xCwwxNbtK6Y?si=MwU0RdTVoJTCdrlL",
+
+        duracao: "6 temporadas",
+        genero: "Ação/Comédia",
+        classificacao: "14",
+    },
+
+    {
+        id: "Lost",
+        titulo: "Lost",
+        imagem: "Imagens/lost.jpg",
+        descricao: "Sobreviventes de um acidente aéreo ficam presos em uma ilha cheia de mistérios e acontecimentos inexplicáveis.",
+        trailer: "oOCaBPbm7KE?si=4MkyXUS2OcRHhSZO",
+
+        duracao: "6 temporadas",
+        genero: "Drama/Mistério",
+        classificacao: "14",
+    },
+
+    {
+        id: "Loki",
+        titulo: "Loki",
+        imagem: "Imagens/loki.jpg",
+        descricao: "Loki se envolve com uma organização responsável por proteger diferentes linhas do tempo.",
+        trailer: "xdBWrkcmMwU?si=kQXRWAht2J100e63",
+
+        duracao: "2 temporadas",
+        genero: "Ação/Ficção científica",
+        classificacao: "12",
+    },
+
+    {
+        id: "Arcane",
+        titulo: "Arcane",
+        imagem: "Imagens/arcane.jpg",
+        descricao: "Duas irmãs acabam em lados diferentes de um conflito entre duas cidades enquanto seus destinos se cruzam.",
+        trailer: "3MB3OK3Xnvs?si=UtA8gQElCGK7-jMF",
+
+        duracao: "2 temporadas",
+        genero: "Animação/Ação",
+        classificacao: "16",
+    },
+
+    {
+        id: "The Flash",
+        titulo: "The Flash",
+        imagem: "Imagens/flash.jpg",
+        descricao: "Barry Allen ganha supervelocidade e passa a usar seus poderes para proteger sua cidade.",
+        trailer: "LJ4nm-D-zGc?si=jQX6xQIzWJIdsj3k",
+
+        duracao: "9 temporadas",
+        genero: "Ação/Super-heróis",
+        classificacao: "12",
+    },
+
+    {
+        id: "One Piece",
+        titulo: "One Piece",
+        imagem: "Imagens/piece.jpg",
+        descricao: "Monkey D. Luffy parte em uma grande aventura pelos mares em busca do lendário tesouro One Piece.",
+        trailer: "YC8SLpnxsL4?si=fMYEmgo9BUyVrVOQ",
+
+        duracao: "Em andamento",
+        genero: "Aventura/Ação",
+        classificacao: "14",
     }
 ];
