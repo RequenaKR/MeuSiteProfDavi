698fa8b649c7b0570a1a38f762b665ddae03f46b
RequenaKR <kaiorequena2009@gmail.com>
2026-09-08 09:05:17 -0300

    filmes e series
    mudei as fotos e coloquei os links dos trailers nos filmes e series.

--- filmes.js
+++ filmes.js
@@ -4,85 +4,85 @@ export const filmes = [
     {
         id: "interestelar",
         titulo: "Interestelar",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/interestelar.jpg",
         descricao: "Um grupo de astronautas viaja através de um buraco de minhoca em busca de um novo lar para a humanidade.",
         trailer: "i6avfCqKcQo?si=c67bqEGle7cgphln",
 
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "2h 49m",
+        genero: "Ficção científica/Aventura",
+        classificacao: "10",
     },
 
     {
         id: "Batman",
         titulo: "Batman",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/batman.png",
         descricao: "Em Batman: O Cavaleiro das Trevas, a paz é ameaçada pelo Coringa, um anarquista que instaura o caos.",
-        trailer: "ID_YOUTUBE",
+        trailer: "zqfz04yCTnE?si=wjDxG2HXrNB4ecaE",
 
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "2h 32m",
+        genero: "Ação/Crime",
+        classificacao: "14",
     },
 
     {
         id: "Ong Bak",
         titulo: "Ong Bak",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/ong bak.jpg",
         descricao: "Ong-Bak: Guerreiro Sagrado, acompanha Ting (Tony Jaa), um jovem guerreiro de uma pacata vila tailandesa.",
-        trailer: "ID_YOUTUBE",
+        trailer: "w-YIX_qsIZ8?si=kN7NGwYwqGMrh-jT",
 
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "1h 48m",
+        genero: "Ação/Thriller",
+        classificacao: "12",
     },
 
     {
         id: "Kung-Fusão",
         titulo: "Kung-Fusão",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/kung.jpg",
         descricao: "A história acompanha Sing, um malandro trapalhão que tenta entrar para a temida Gangue do Machado.",
-        trailer: "ID_YOUTUBE",
+        trailer: "FtE9-o6dBEI?si=HR-nldNrs90Fe2rt",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "1h 39m",
+        genero: "Ação/Comédia",
+        classificacao: "14",
     },
 
     {
         id: "Homem Aranha",
         titulo: "Homem Aranha",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/spider.jpg",
         descricao: "O jovem Peter Parker que, após ser picado por uma aranha, ganha superpoderes como força, agilidade e a capacidade de escalar paredes.",
-        trailer: "ID_YOUTUBE",
+        trailer: "PlulyWs1kS4?si=CDWsOFlWp8_ismni",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "2h 30m",
+        genero: "Ação/Aventura",
+        classificacao: "12",
     },
 
     {
         id: "Capitão América: Guerra Civil",
         titulo: "Capitão América: Guerra Civil",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/cpt.jpg",
         descricao: "Após danos colaterais em missões dos Vingadores, a ONU propõe o Tratado de Sokovia, um acordo para regulamentar e controlar a equipe.",
-        trailer: "ID_YOUTUBE",
+        trailer: "FRLYZn5PGuA?si=OOMR0ZLV34CEcAst",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "2h 28m",
+        genero: "Ação/Aventura",
+        classificacao: "12",
     }
 ];
 
@@ -90,79 +90,79 @@ export const series = [
     {
         id: "Stranger Things",
         titulo: "Stranger Things",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/st.jpg",
         descricao: "Um grupo de crianças enfrenta forças sobrenaturais.",
-        trailer: "ID_YOUTUBE",
+        trailer: "Wb3kaP580kA?si=hV-uIBRlsxDDCpY3",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "5 temporadas",
+        genero: "Terror",
+        classificacao: "18",
     },
     {
         id: "Dark",
         titulo: "Dark",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/dark.jpg",
         descricao: "Dark é uma série focada em viagens no tempo e multiversos.",
-        trailer: "ID_YOUTUBE",
+        trailer: "JCCssUOtn2E?si=fn1qyfyPe2za_bV0",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "3 temporadas",
+        genero: "Thriller",
+        classificacao: "16",
     },
     {
         id: "Breaking Bad",
         titulo: "Breaking Bad",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/bd.jpg",
         descricao: " A série acompanha Walter White e a sua transformação de um homem comum no chefão do tráfico.",
-        trailer: "ID_YOUTUBE",
+        trailer: "2gTC4uWP3_Y?si=Yjs5nM_Gu5yW22WK",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "5 temporadas",
+        genero: "Drama",
+        classificacao: "16",
     },
     {
         id: "Peaky Blinders",
         titulo: "Peaky Blinders",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/pb.webp",
         descricao: "Peaky Blinders acompanha Thomas Shelby e sua gangue em Birmingham após a Primeira Guerra Mundial.",
-        trailer: "ID_YOUTUBE",
+        trailer: "wQHSX7x8Bm0?si=30eywh79mdksOKFV",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "6 temporadas",
+        genero: "Ficção policial",
+        classificacao: "18",
     },
     {
         id: "Round 6",
         titulo: "Round 6",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/round6.webp",
         descricao: "Centenas de jogadores falidos aceitam um estranho convite para um jogo de sobrevivência. Onde um prêmio milionário aguarda.",
-        trailer: "ID_YOUTUBE",
+        trailer: "6zp3mt5Dd2c?si=b-yv0wTefkigzPy7",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "3 temporadas",
+        genero: "Thriller",
+        classificacao: "18",
     },
     {
         id: "Avatar A lenda de Aang",
         titulo: "Avatar A lenda de Aang",
-        imagem: "Imagens/imagem.jpg",
+        imagem: "Imagens/avatar.webp",
         descricao: "Avatar: A Lenda de Aang acompanha Aang, um menino de 12 anos que acorda após cem anos congelado em um iceberg.",
-        trailer: "ID_YOUTUBE",
+        trailer: "8E5oyWQ3Ze0?si=pV8u1fkSb1x0Tjw_",
         
         // ALTERAÇÃO 21/08 - INFORMAÇÕES COMPLEMENTARES
 
-        duracao: "PREENCHER",
-        genero: "PREENCHER",
-        classificacao: "PREENCHER",
+        duracao: "3 temporadas",
+        genero: "Aventura",
+        classificacao: "10",
     }
 ];
